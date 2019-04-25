5cd6ad7daee63cdbb1ef628d113667957a51d7a6
Chris Stewart <stewart.chris1234@gmail.com>
2019-04-25 17:50:02 -0500

    First commit for implementing a BlockchainBuilder

--- chain-test/src/test/scala/org/bitcoins/chain/util/ChainUnitTest.scala
+++ chain-test/src/test/scala/org/bitcoins/chain/util/ChainUnitTest.scala
@@ -47,7 +47,7 @@ trait ChainUnitTest
   lazy val blockchain: Blockchain =
     Blockchain.fromHeaders(Vector(genesisHeaderDb), blockHeaderDAO)
 
-  private lazy val chainHandler: ChainHandler = ChainHandler(blockchain)
+  private lazy val chainHandler: ChainHandler = ChainHandler(blockchain = blockchain,chainAppConfig = appConfig)
 
   implicit def ec: ExecutionContext =
     scala.concurrent.ExecutionContext.Implicits.global
