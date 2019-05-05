ec6440d3ccd91722b10a2da42633ff4102896939
Chris Stewart <stewart.chris1234@gmail.com>
2019-05-05 09:50:55 -0500

    Add script to illustrate how the chain persisted and how to sync against a running bitcoind instance on regtest

--- chain/src/main/scala/org/bitcoins/chain/config/ChainAppConfig.scala
+++ chain/src/main/scala/org/bitcoins/chain/config/ChainAppConfig.scala
@@ -1,6 +1,25 @@
 package org.bitcoins.chain.config
 
 import org.bitcoins.chain.db.ChainDbConfig
+import org.bitcoins.chain.models.BlockHeaderDAO
 import org.bitcoins.db.AppConfig
 
-case class ChainAppConfig(dbConfig: ChainDbConfig) extends AppConfig[ChainDbConfig]
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
+
+case class ChainAppConfig(dbConfig: ChainDbConfig) extends AppConfig[ChainDbConfig] {
+
+  def isDbInitialized()(implicit ec: ExecutionContext): Future[Boolean] = {
+    val bhDAO = BlockHeaderDAO(this)
+    val p = Promise[Boolean]()
+    val isDefinedOptF = {
+      bhDAO.read(chain.genesisBlock.blockHeader.hashBE).map(_.isDefined)
+    }
+    isDefinedOptF.onComplete {
+      case Success(bool) => p.success(bool)
+      case Failure(err) => p.success(false)
+    }
+
+    p.future
+  }
+}
